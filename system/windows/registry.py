a4ade66152877872ce040d4b079e39a07dbbb0cb
Kingen <wsg787@126.com>
2024-06-28 19:28:10 +0800

    bugfix

--- system/windows/registry.py
+++ system/windows/registry.py
@@ -12,24 +12,20 @@ import sys
 import winreg
 from tkinter import messagebox
 
-import common
-
-log = common.create_logger(__name__)
-
 CMD_DELEGATE_KEY = 'DelegateExecute'
 
 
 def register_protocol(protocol: str, delegate: str):
     if not protocol or protocol.strip() == '':
-        log.warning('protocol is empty')
+        messagebox.showwarning('Warning', 'Protocol is empty')
         return
     protocol = protocol.strip()
     if not re.match(r'^[a-z]+$', protocol):
-        log.warning('protocol only accepts lowercase letters: [%s]', protocol)
+        messagebox.showwarning('Warning', f'Protocol only accepts lowercase letters: [{protocol}]')
         return
     delegate = os.path.abspath(delegate)
     if not os.path.exists(delegate):
-        log.warning('delegate executable does not exist: [%s]', delegate)
+        messagebox.showwarning('Warning', f'Delegate executable does not exist: [{delegate}]')
         return
     transfer = os.path.abspath(sys.argv[0])
 
@@ -42,18 +38,18 @@ def register_protocol(protocol: str, delegate: str):
     command_key = winreg.CreateKey(open_key, "command")
     winreg.SetValueEx(command_key, None, 0, winreg.REG_SZ, f'"{transfer}" open {protocol} "%1"')
     winreg.SetValueEx(command_key, CMD_DELEGATE_KEY, 0, winreg.REG_SZ, f'{delegate}')
-    log.info('protocol [%s] was registered with delegate executable [%s]', protocol, delegate)
+    messagebox.showinfo('Success', f'Protocol [{protocol}] was registered with delegate executable [{delegate}]')
 
 
 def open_url(protocol: str, url: str):
     if not protocol or protocol.strip() == '':
-        log.warning('protocol is empty')
+        messagebox.showwarning('Warning', 'Protocol is empty')
     protocol = protocol.strip()
     if not url or url.strip() == '':
-        log.warning('url is empty')
+        messagebox.showwarning('Warning', 'The URL is empty')
     url = url.strip()
     if not url.startswith(protocol + '://'):
-        log.warning("the url doesn't match the protocol: [%s]", protocol)
+        messagebox.showwarning('Warning', f'The URL [{url}] does not match the protocol [{protocol}]')
     url = url[len(protocol) + 3:]
 
     protocol_key = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, protocol)
@@ -62,18 +58,15 @@ def open_url(protocol: str, url: str):
     command_key = winreg.OpenKey(open_key, "command")
     delegate = winreg.QueryValueEx(command_key, CMD_DELEGATE_KEY)[0]
     if delegate == '':
-        log.warning('the delegate executable is not set')
         messagebox.showwarning('Warning', 'Cannot find an executable to open the url')
         return
-
-    log.info('open url [%s] with [%s]', url, delegate)
     subprocess.Popen([delegate, url])
 
 
 # pyinstaller -n registry -w -F registry.py
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        log.error('at least 2 arguments are required')
+        messagebox.showwarning('Warning', 'At least 2 arguments are required')
     elif len(sys.argv) == 3:
         register_protocol(sys.argv[1], sys.argv[2])
     else:
@@ -83,4 +76,4 @@ if __name__ == '__main__':
         elif cmd == 'open':
             open_url(sys.argv[2], sys.argv[3])
         else:
-            log.error('unknown command: [%s]', cmd)
+            messagebox.showwarning('Warning', f'Unknown command: [{cmd}]', )
